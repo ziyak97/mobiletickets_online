1ee5c92e92fae5ee5dceb9455350aa28334dbc82
Ziyak Jehangir <ziyak97@gmail.com>
2021-06-12 22:59:40 +0530

    feat: added pdf embedding

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -7,7 +7,24 @@ const Home: React.FC<AppProps> = () => {
   const router = useRouter()
   const { id } = router.query
 
-  if (!id) return <div>Illegal</div>
+  if (!id)
+    return (
+      <div className={styles.container}>
+        <object
+          data="https://firebasestorage.googleapis.com/v0/b/mobiletickets-online.appspot.com/o/test.pdf?alt=media&token=79e434f3-913f-46c7-b457-57b2eb14bbc5"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+        >
+          <embed
+            src="https://firebasestorage.googleapis.com/v0/b/mobiletickets-online.appspot.com/o/test.pdf?alt=media&token=79e434f3-913f-46c7-b457-57b2eb14bbc5"
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          />
+        </object>
+      </div>
+    )
 
   return <div className={styles.container}></div>
 }
