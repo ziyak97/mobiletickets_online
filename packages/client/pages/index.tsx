ace04348973a0ac0f7c007a64d5eb69b66753332
Ziyak Jehangir <53836911+ziyak97@users.noreply.github.com>
2021-08-29 01:29:51 +0530

    test

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -56,7 +56,7 @@ const Home: React.FC<AppProps> = () => {
 
   return (
     <div className={styles.container}>
-      <iframe src={`http://docs.google.com/gview?embedded=true&${pdfUrl}`} style={{minHeight: '100%'}} width="100%" ></iframe>
+      <iframe src={`https://docs.google.com/gview?embedded=true&url=${pdfUrl}`} style={{minHeight: '100%'}} width="100%" ></iframe>
       {/* <Loader show type="circle" center style={{ position: 'absolute', zIndex: -1 }} /> */}
       {/* <object data={pdfUrl} type="application/pdf" width="100%" >
         <embed src={pdfUrl} type="application/pdf" width="100%" />
