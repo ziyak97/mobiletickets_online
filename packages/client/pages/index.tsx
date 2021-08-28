3d3ec9c0d040e29804941a134043c82aac75896e
Ziyak Jehangir <53836911+ziyak97@users.noreply.github.com>
2021-08-29 00:34:33 +0530

    style changes

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -54,13 +54,9 @@ const Home: React.FC<AppProps> = () => {
   return (
     <div className={styles.container}>
       {/* <Loader show type="circle" center style={{ position: 'absolute', zIndex: -1 }} /> */}
-      <iframe src={pdfUrl} style={{minHeight: '100%'}} width="100%" >
+      {/* <object data={pdfUrl} type="application/pdf" width="100%" >
         <embed src={pdfUrl} type="application/pdf" width="100%" />
-      </iframe>
-<!-- 
-      <object data={pdfUrl} type="application/pdf" width="100%" >
-        <embed src={pdfUrl} type="application/pdf" width="100%" />
-      </object> -->
+      </object> */}
     </div>
   )
 }
